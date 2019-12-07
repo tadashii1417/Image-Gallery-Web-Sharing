042ecebd6ec53a9b43d2de1026ea59d2aa462a51
tadashii1417 <tadashii1417@gmail.com>
2019-12-07 16:18:08 +0700

    add admin page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import LoginForm from "../containers/Login/Login";
 import Register from "../containers/Register/Register";
 import NotFound from '../components/notFound/notFound';
 import Category from "../containers/Category/category";
+import ResutlWelcome from "../components/ResultWelcome/ResutlWelcome";
+import Admin from "../containers/Admin/Admin";
 
 
 export default function (props) {
@@ -16,6 +18,7 @@ export default function (props) {
                 <Route path="/login" component={LoginForm} exact/>
                 <Route path="/register" component={Register} exact/>
                 <Route path="/category/:id" component={Category} exact/>
+                <Route path="/admin" component={Admin}/>
                 <Route component={NotFound}/>
             </Switch>
         </BrowserRouter>
